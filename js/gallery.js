3c3aa74da2da0952133736d2bd8c1e294b63fa2c
Raul Gonzalez <54822905+raulgonzalez77@users.noreply.github.com>
2024-04-13 05:56:27 -0500

    added styles

--- js/gallery.js
+++ js/gallery.js
@@ -3,9 +3,19 @@ $(document).ready(function () {
     //console.log("ready!");
 
     $("#blinn-gallery").nanogallery2({
-        ...
-        thumbnailHeight: 200,
-        thumbnailWidth: 200
+        thumbnailWidth: "300",
+        thumbnailHeight: "auto",
+        thumbnailBorderVertical: 0,
+        thumbnailBorderHorizontal: 0,
+        thumbnailLabel: {
+            postition: "overImageOnBottom",
+            display: false
+        },
+        thumbnailHoverEffect2: "imageScaleIn80|imageSepiaOff|labelAppear75",
+        thumbnailGutterWidth: 0,
+        thumbnailGutterHeight: 0,
+        thumbnailAlingnment: "center",
+        thumbnailOpenImage: true
     });
 
 });
