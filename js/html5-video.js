9ea6d692074f313bb1a374955a9aa9090b6fb45a
Raul Gonzalez <54822905+raulgonzalez77@users.noreply.github.com>
2024-06-03 17:48:24 -0500

    updates

--- js/html5-video.js
+++ js/html5-video.js
@@ -81,6 +81,21 @@ $(document).ready(function () {
 
 
 
+    /* Get the element you want displayed in fullscreen */
+    var elem = document.getElementById("callToAction");
+
+    /* Function to open fullscreen mode */
+    function openFullscreen() {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+        }
+    }
+
+
 
 
 });
