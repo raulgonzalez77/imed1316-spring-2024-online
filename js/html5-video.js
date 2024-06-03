0f71397f2ba77a38a0e0c00c71ab6c269a8938dc
Raul Gonzalez <54822905+raulgonzalez77@users.noreply.github.com>
2024-06-03 16:49:24 -0500

    updates

--- js/html5-video.js
+++ js/html5-video.js
@@ -3,21 +3,22 @@ $(document).ready(function () {
 
     var video = document.getElementById('video');
 
-    document.querySelector('video').addEventListener('click', function () {
-        if (video.paused) {
-
-            $(".content").removeClass("content-video");
-            $(".call-to-action").css('display', 'none');
-
-        }
-        else {
-
-            $(".content").addClass("content-video");
-            $(".call-to-action").css('display', 'block');
-        }
-
-    }, false);
-
+    /* 
+     document.querySelector('video').addEventListener('click', function () {
+         if (video.paused) {
+ 
+             $(".content").removeClass("content-video");
+             $(".call-to-action").css('display', 'none');
+ 
+         }
+         else {
+ 
+             $(".content").addClass("content-video");
+             $(".call-to-action").css('display', 'block');
+         }
+ 
+     }, false);
+ */
 
     /*
         video.addEventListener("playing", (event) => {
