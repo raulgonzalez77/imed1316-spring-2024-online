0152c0c82faa3077eaa2cbbd04a13b5ac82412c2
Raul Gonzalez <54822905+raulgonzalez77@users.noreply.github.com>
2024-06-05 13:43:42 -0500

    updates

--- js/sanjac.js
+++ js/sanjac.js
@@ -21,28 +21,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
                     alert('you chose sociology');
 
-                    /*     */
                     var selEl = document.getElementById("input-1");
                     document.getElementById('input-1').value='Sociology';
                     //var selectElem = document.querySelector('#input-1');
                     selEl.dispatchEvent(new Event('change'));
             
-                    
-                    //var selEl = document.getElementById("input-1");
-                    //document.getElementById('input-1').value='Sociology';
-                    //selEl.options[1].selected = true;
-                    //selEl.onchange();
-
-            /*
-                    const selectElement = document.querySelector("#input-1");
-                    const result = document.querySelector(".result");
-
-                    selectElement.addEventListener("change", (event) => {
-                        alert('changed');
-                    selectElement.dispatchEvent(new Event('change'));
-                    });
-            */
-
 
             }, 5000);
 
