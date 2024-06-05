cec57d052d13677040ac492eceaeda9f2f74681d
Raul Gonzalez <54822905+raulgonzalez77@users.noreply.github.com>
2024-06-05 13:39:31 -0500

    updates

--- js/sanjac.js
+++ js/sanjac.js
@@ -16,30 +16,35 @@ document.addEventListener('DOMContentLoaded', function () {
     const dirID = urlParams.get('dirID')
 
     if (dirID == 'Sociology') {
-        alert('you chose sociology');
 
-        /*     */
-        var selEl = document.getElementById("input-1");
-        document.getElementById('input-1').value='Sociology';
-        //var selectElem = document.querySelector('#input-1');
-        selEl.dispatchEvent(new Event('change'));
-   
-        
-        //var selEl = document.getElementById("input-1");
-        //document.getElementById('input-1').value='Sociology';
-        //selEl.options[1].selected = true;
-        //selEl.onchange();
+            setTimeout(function() {
 
-/*
-        const selectElement = document.querySelector("#input-1");
-        const result = document.querySelector(".result");
+                    alert('you chose sociology');
 
-        selectElement.addEventListener("change", (event) => {
-            alert('changed');
-        selectElement.dispatchEvent(new Event('change'));
-        });
-*/
+                    /*     */
+                    var selEl = document.getElementById("input-1");
+                    document.getElementById('input-1').value='Sociology';
+                    //var selectElem = document.querySelector('#input-1');
+                    selEl.dispatchEvent(new Event('change'));
+            
+                    
+                    //var selEl = document.getElementById("input-1");
+                    //document.getElementById('input-1').value='Sociology';
+                    //selEl.options[1].selected = true;
+                    //selEl.onchange();
 
+            /*
+                    const selectElement = document.querySelector("#input-1");
+                    const result = document.querySelector(".result");
+
+                    selectElement.addEventListener("change", (event) => {
+                        alert('changed');
+                    selectElement.dispatchEvent(new Event('change'));
+                    });
+            */
+
+
+            }, 5000);
 
     }    
 
