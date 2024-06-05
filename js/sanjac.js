cb2cadcadae98b20d6c47cefde782dbceab1d38f
Raul Gonzalez <54822905+raulgonzalez77@users.noreply.github.com>
2024-06-05 13:08:19 -0500

    updates

---
+++ js/sanjac.js
@@ -0,0 +1,33 @@
+//$( document ).ready(function() {
+document.addEventListener('DOMContentLoaded', function () {
+  // do something here ...    
+  
+    var cur_url = window.location.href;
+    
+    
+    
+    
+    
+    //////////////////getting parameters into variable values to pass to PHP page///////////////
+    const queryString = window.location.search;
+    
+    const urlParams = new URLSearchParams(queryString);
+
+    const dirID = urlParams.get('dirID')
+
+    if (dirID == 'Sociology') {
+        alert('you chose sociology');
+
+        document.getElementById('input-1').value='Sociology';
+
+        var selectElem = document.querySelector('"#input-1"');
+        selectElem.onchange();
+
+    }    
+
+
+
+    
+}, false);
+	
+//}); //document ready		
